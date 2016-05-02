93ca532320457014198738e04e70ca0507d0a457
Arun <arun@in-arunsathin.local>
2016-05-02 12:48:50 +0530

    made it more user interactive throughterminal

--- playWithDice/Board.rb
+++ playWithDice/Board.rb
@@ -10,33 +10,61 @@ class Board
     @currentPosition = -5
   end
 
-  def moveTheCoin(number)
+
+  def moveTheCoin number
     @totalTurns+=1
-    puts "the dice value is #{number}"
-    if number == 6
-      @gameStart = true
-    end
+    puts "The dice value is #{number}"
+    checkGameStart number
     return "Need a 6 to start the journey...!!" if !@gameStart
-    if (@currentPosition += number)>16
-      @currentPosition-= number
-    end
+    determineGameStatus(number)
     puts @@cells[@currentPosition]
   end
 
+
   def play
-    while @currentPosition <16 do
+    if @currentPosition <16
       puts moveTheCoin(@player.roll_dice)
-      puts "current position is #{@currentPosition}"
+      puts "current position is #{@currentPosition}\n"
+    else
+      puts("you won......!!")
+      @@gameRecord[@player.playerName] = @totalTurns
+      puts "number of turns you took to finish  #{@totalTurns}"
+
     end
-    @@gameRecord[@player.playerName] = @totalTurns
-    puts "number of turns you took to finish  #{@totalTurns}"
+  end
+
+  private
+  def determineGameStatus number
+    check = (@currentPosition+=number) <=>16
+    if check ==0
+      puts "you won........!!"
+    else
+      if check == -1
+        return @currentPosition
+      else
+        if check ==1
+          return @currentPosition-=number
+        end
+      end
+    end
+  end
+
+  def checkGameStart number
+    if number == 6
+      @gameStart = true
+    end
   end
 
 end
+
+
 player1 = Player.new('suresh')
 player2 = Player.new('ramesh')
 board1 = Board.new(player1)
 board2 = Board.new(player2)
-board1.play
-board2.play
-puts Board.class_variable_get(:@@gameRecord)
+while !(player1.gameStatus && player2.gameStatus) do
+  puts("#{player1.playerName} 's turn\n Please roll the dice")
+  board1.play
+  puts("#{player2.playerName} 's turn\n Please roll the dice")
+  board2.play
+end
--- playWithDice/Player.rb
+++ playWithDice/Player.rb
@@ -1,12 +1,18 @@
 class Player
-  attr_accessor :playerName
+attr_accessor :playerName,:gameStatus
 
   def initialize(playerName)
+    @gameStatus = false
     @playerName = playerName
   end
 
+
   def roll_dice
-    1 + rand(6)
+    flag = gets
+    if flag
+      flag = false
+      1 + rand(6)
+    end
   end
 end
 
